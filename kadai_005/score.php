7f50a0403953edde108e7c1ca224fdb37adf6285
setsuki-kamiya <165028453+setsuki-kamiya@users.noreply.github.com>
2024-07-12 08:24:33 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -18,6 +18,8 @@
   $score9 = 30;
   $score10 = 60;
 
+  $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
   echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) /10;
   ?>
 </p>
