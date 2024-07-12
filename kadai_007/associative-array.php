d0f9a20225fe579f28db9c0e98a4355d633257aa
setsuki-kamiya <165028453+setsuki-kamiya@users.noreply.github.com>
2024-07-12 19:52:15 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<header>
+  <meta charset="UTF-8">
+  <title>連想配列を使ってキーと値を出力しよう</title>
+</header>
+
+<body>
+  <p>
+    <?php
+    $personal_date = ['name' => 'onion', 'price' => 200, 'weight'=>160];
+
+    print_r($personal_date);
+    ?>
+  </p>
+</body>
+
+</html>
